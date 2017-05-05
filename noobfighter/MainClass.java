a30c0d279275c62fd606b2fa6a2a0e528c6b660b
aaron <realemail1132@gmailcom>
2017-05-05 16:20:50 -0400

    'message'

---
+++ noobfighter/MainClass.java
@@ -0,0 +1,13 @@
+package noobfighter;
+
+import org.tribot.script.Script;
+
+public class MainClass extends Script {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
